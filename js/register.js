27004b3d8f43fe8231146e3d77155bad16a8b9b5
Somsubhra Das <das.somsubhra1@gmail.com>
2019-01-12 01:10:41 +0530

    Registration form major redesign

--- js/register.js
+++ js/register.js
@@ -1,3 +1,6 @@
+var member1 = document.getElementById('member1');
+var member2 = document.getElementById('member2');
+var member3 = document.getElementById('member3');
 var member4 = document.getElementById('member4');
 var member5 = document.getElementById('member5');
 
@@ -7,8 +10,23 @@ document.getElementById("registrationForm").addEventListener('submit', submitted
 
 function eventSelected(event) {
     if (event.value === 'coding' || event.value === 'quiz') {
+        member1.style.display = "block";
+        member2.style.display = "block";
+        member3.style.display = "block";        
         member4.style.display = 'none';
         member5.style.display = 'none';
+
+        document.getElementById("member1-name").required = true;
+        document.getElementById("member1-class").required = true;
+        document.getElementById("member1-contact").required = true;
+
+        document.getElementById("member2-name").required = true;
+        document.getElementById("member2-class").required = true;
+        document.getElementById("member2-contact").required = true;
+
+        document.getElementById("member3-name").required = true;
+        document.getElementById("member3-class").required = true;
+        document.getElementById("member3-contact").required = true;
 
         document.getElementById("member4-name").required = false;
         document.getElementById("member4-class").required = false;
@@ -19,8 +37,23 @@ function eventSelected(event) {
         document.getElementById("member5-contact").required = false;
     }
     else if (event.value === 'model') {
+        member1.style.display = "block";
+        member2.style.display = "block";
+        member3.style.display = "block";
         member4.style.display = "block";
-        member5.style.display = "block"; 
+        member5.style.display = "block";
+        
+        document.getElementById("member1-name").required = true;
+        document.getElementById("member1-class").required = true;
+        document.getElementById("member1-contact").required = true;
+
+        document.getElementById("member2-name").required = true;
+        document.getElementById("member2-class").required = true;
+        document.getElementById("member2-contact").required = true;
+
+        document.getElementById("member3-name").required = true;
+        document.getElementById("member3-class").required = true;
+        document.getElementById("member3-contact").required = true;
 
         document.getElementById("member4-name").required = true;
         document.getElementById("member4-class").required = true;
@@ -29,6 +62,13 @@ function eventSelected(event) {
         document.getElementById("member5-name").required = true;
         document.getElementById("member5-class").required = true;
         document.getElementById("member5-contact").required = true;
+    }
+    else {
+        document.getElementById("member1").style.display = "none";
+        document.getElementById("member2").style.display = "none";
+        document.getElementById("member3").style.display = "none";
+        document.getElementById("member4").style.display = "none";
+        document.getElementById("member5").style.display = "none";
     }
      
 }
