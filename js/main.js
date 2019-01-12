387bbf9a13653a7c13a7bc4f80e81ff314396363
Somsubhra Das <das.somsubhra1@gmail.com>
2019-01-13 01:41:58 +0530

    Added slider parallax

--- js/main.js
+++ js/main.js
@@ -7,4 +7,21 @@ var swiper = new Swiper('.swiper-container', {
     keyboard: {
       enabled: true,
     },
-  });
+});
+  
+$(window).parallaxmouse({
+  invert: true,
+  range: 400,
+  elms: [
+    { el: $('#warpbg'), rate: 0.1 },
+    { el: $('#star1'), rate: 0.2 },
+    { el: $('#star2'), rate: 0.2 },
+    { el: $('#star3'), rate: 0.2 },
+    { el: $('#star4'), rate: 0.2 },
+    { el: $('#star5'), rate: 0.2 },
+    { el: $('#planet'), rate: 0.3 },
+    { el: $('#robot'), rate: 0.4 },
+    { el: $('#astronaut'), rate: 0.48 },
+    { el: $('#asteroid'), rate: 0.7 }
+  ]
+});
